b890249e1926d895adbde54efff16134fc111237
Lemnul <andreilemnaru73@gmail.com>
2023-11-02 13:48:46 +0200

    Quiz

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,111 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+type Question = {
+  prompt: string;
+  corectAnswer: string;
+  answers: string[];
+};
+
+const question: Question[] = [
+  {
+    prompt: "What color is the sky?",
+    corectAnswer: "blue",
+    answers: ["blue", "red", "yellow", "green"],
+  },
+  {
+    prompt: "What is the Chemical Formula of water?",
+    corectAnswer: "H2O",
+    answers: ["NaCl", "H2O", "CaCO2", "C7H5N3O6"],
+  },
+  {
+    prompt: "Who directed the movie Interselar?",
+    corectAnswer: "Cristopher Nolan",
+    answers: [
+      "Brad Pitt",
+      "Lemnaru Andrei",
+      "Cristopher Nolan",
+      "Quentin Tarantino",
+    ],
+  },
+  {
+    prompt: "Care-i cea mai buna bere?",
+    corectAnswer: "Aia care iti place",
+    answers: ["Noroc", "Aia care iti place", "Tuborg", "Guinness"],
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+  const [score, setScore] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  console.log(selectedAnswer);
+
+  const currentQuestion = question[currentQuestionIndex];
+
+  const gameOver = currentQuestionIndex >= question.length;
+
+  function Quiz() {
+    return (
+      <div className="page">
+        <h1>{currentQuestion.prompt}</h1>
+        <form
+          className="form"
+          onSubmit={(e) => {
+            e.preventDefault();
+
+            if (selectedAnswer === currentQuestion.corectAnswer) {
+              setScore(score + 1);
+            }
+            setCurrentQuestionIndex(currentQuestionIndex + 1);
+          }}
+        >
+          {currentQuestion.answers.map((answer) => {
+            return (
+              <label className="answer" key={answer}>
+                <input
+                  onChange={() => setSelectedAnswer(answer)}
+                  type="radio"
+                  name="answer"
+                  checked={answer === selectedAnswer}
+                ></input>
+                {answer}
+              </label>
+            );
+          })}
+          <button className="submitButton">Submit</button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    );
+  }
+
+  function ScoreScreen() {
+    const wrongAnswers = question.length - score;
+
+    return (
+      <>
+        <div>
+          Your score {score}, you got{" "}
+          {wrongAnswers === 1
+            ? wrongAnswers + " question wrong"
+            : wrongAnswers + " questions wrong"}
+          .
+        </div>
+        <button
+          className="resetButton"
+          onClick={() => {
+            setCurrentQuestionIndex(0);
+            setScore(0);
+          }}
+        >
+          Reset
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </>
+    );
+  }
+
+  return <div>{gameOver ? <ScoreScreen /> : <Quiz />}</div>;
 }
 
-export default App
+export default App;
